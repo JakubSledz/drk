33fa2cfcb27e2412c594793334b0e3d8afee5ad0
TheLinghos <jakub.sledz@pogaduszki.pl>
2023-08-01 15:42:58 +0200

    Update drkjs.js

--- drkjs.js
+++ drkjs.js
@@ -73,6 +73,7 @@ $(document).ready(function () {
   });
   $("#reset_quiz").click(function (e) {
     e.preventDefault();
+
     $("div.surv_slider_nav.w-slider-nav div:nth-child(1)").trigger("tap");
   });
 });
