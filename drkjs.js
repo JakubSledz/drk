275cb1c01199882b718859d5a5506165b49b49f4
TheLinghos <jakub.sledz@pogaduszki.pl>
2023-08-02 08:47:13 +0200

    Update drkjs.js

--- drkjs.js
+++ drkjs.js
@@ -6,18 +6,21 @@ document.addEventListener("DOMContentLoaded", function () {
       .querySelector("div.factions_slider_nav.w-slider-nav div:nth-child(1)")
       .dispatchEvent(new Event("tap"));
   });
+
   document.getElementById("2").addEventListener("click", function (e) {
     e.preventDefault();
     document
       .querySelector("div.factions_slider_nav.w-slider-nav div:nth-child(2)")
       .dispatchEvent(new Event("tap"));
   });
+
   document.getElementById("3").addEventListener("click", function (e) {
     e.preventDefault();
     document
       .querySelector("div.factions_slider_nav.w-slider-nav div:nth-child(3)")
       .dispatchEvent(new Event("tap"));
   });
+
   document.getElementById("4").addEventListener("click", function (e) {
     e.preventDefault();
     document
@@ -37,20 +40,40 @@ document.addEventListener("DOMContentLoaded", function () {
     };
   }
 
-  for (let i = 1; i <= 11; i++) {
-    const quizElements = document.querySelectorAll(
-      `#one-${i}, #two-${i}, #three-${i}, #four-${i}, #five-${i}, #six-${i}, #seven-${i}, #eight-${i}, #nine-${i}, #ten-${i}`
-    );
-    quizElements.forEach(function (element) {
-      element.addEventListener("click", triggerQuizNav(i + 1));
+  for (let i = 1; i <= 10; i++) {
+    const questionIds = [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+    ];
+    questionIds.forEach(function (questionId) {
+      document
+        .getElementById(`${questionId}-${i}`)
+        .addEventListener("click", triggerQuizNav(i + 1));
     });
   }
 
+  // Handling quiz reset
   document.getElementById("reset_quiz").addEventListener("click", function (e) {
     e.preventDefault();
-    document
-      .querySelector("div.surv_slider_nav.w-slider-nav div:nth-child(1)")
-      .dispatchEvent(new Event("tap"));
+    const firstSlide = document.querySelector(
+      "div.surv_slider_nav.w-slider-nav div:nth-child(1)"
+    );
+    firstSlide.dispatchEvent(new Event("tap"));
+    localStorage.clear();
+
+    // Removing the "selected" class from all elements with the class "quiz-a"
+    const quizAnswers = document.querySelectorAll(".quiz-a");
+    quizAnswers.forEach(function (answer) {
+      answer.classList.remove("selected");
+    });
   });
 
   // Setting "tab_active" class on element with ID "1"
@@ -67,8 +90,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const tabMenus = document.querySelectorAll(".tab_menu .tab");
   tabMenus.forEach(function (tab) {
     tab.addEventListener("click", function () {
-      const tabLine = tab.querySelector(".tab_line .tabline");
-      const tabShape = tab.querySelector(".tab_line .tab_shape");
       document
         .querySelectorAll(".tab_menu .tab_line .tab_shape")
         .forEach(function (el) {
@@ -79,8 +100,8 @@ document.addEventListener("DOMContentLoaded", function () {
         .forEach(function (el) {
           el.classList.remove("tab_active");
         });
-      tabLine.classList.add("tab_active");
-      tabShape.classList.add("tab_active");
+      tab.querySelector(".tab_line .tabline").classList.add("tab_active");
+      tab.querySelector(".tab_line .tab_shape").classList.add("tab_active");
     });
 
     tab.addEventListener("mouseout", function () {
@@ -103,18 +124,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const quizQuestions = [
     { questionIds: ["one", "two", "three", "four"] },
     { questionIds: ["five", "six", "seven", "eight", "nine", "ten"] },
-    { questionIds: ["eleven"] },
   ];
 
   quizQuestions.forEach(function (questionSet, index) {
     const answerName = `answer${index + 1}`;
     questionSet.questionIds.forEach(function (questionId) {
-      const elements = document.querySelectorAll(
-        `#${questionId}-one, #${questionId}-two, #${questionId}-three, #${questionId}-four`
-      );
-      elements.forEach(function (element) {
-        saveQuizAnswer(element, answerName);
-      });
+      for (let i = 1; i <= 4; i++) {
+        const elements = document.querySelectorAll(`#${questionId}-${i}`);
+        elements.forEach(function (element) {
+          saveQuizAnswer(element, answerName);
+        });
+      }
     });
   });
 
@@ -147,75 +167,85 @@ document.addEventListener("DOMContentLoaded", function () {
   document
     .querySelector(".check-answer")
     .addEventListener("click", function () {
-      // Get answers from localStorage
-      const answers = Array.from({ length: 11 }, (_, i) =>
-        Number(localStorage.getItem(`answer${i + 1}`))
-      );
+      // Array of numbers
+      const answers = [
+        Number(localStorage.getItem("answer1")),
+        Number(localStorage.getItem("answer2")),
+        Number(localStorage.getItem("answer3")),
+        Number(localStorage.getItem("answer4")),
+        Number(localStorage.getItem("answer5")),
+        Number(localStorage.getItem("answer6")),
+        Number(localStorage.getItem("answer7")),
+        Number(localStorage.getItem("answer8")),
+        Number(localStorage.getItem("answer9")),
+        Number(localStorage.getItem("answer10")),
+      ];
 
-      // Function to calculate the most frequent number
+      // Function to calculate the most frequent number in the array
       function mostFrequent(arr) {
         const counts = { 1: 0, 2: 0, 3: 0, 4: 0 };
         let maxKey = 1;
 
-        for (const num of arr) {
-          if (num >= 1 && num <= 4) {
-            counts[num]++;
-            if (counts[num] > counts[maxKey]) {
-              maxKey = num;
+        for (let i = 0; i < arr.length; i++) {
+          const key = arr[i];
+          if (key >= 1 && key <= 4) {
+            counts[key]++;
+            if (counts[key] > counts[maxKey]) {
+              maxKey = key;
             }
           }
         }
         return maxKey;
       }
 
-      // Get the most frequent answer
-      const mostFrequentAnswer = mostFrequent(answers);
+      // Get the most frequent number from the answers array
+      const mostFrequentNumber = mostFrequent(answers);
 
-      // Display the result
-      const copyElement = document.querySelector(".answer-value");
-      const logoElement = document.querySelector(".answer-logo");
-
-      switch (mostFrequentAnswer) {
+      // Set the appropriate copy and class based on the most frequent number
+      let copy;
+      switch (mostFrequentNumber) {
         case 1:
-          copyElement.textContent = "Hedonists";
-          logoElement.className = "answer-logo hedo";
+          copy = "Hedonists";
+          document.querySelectorAll(".answer-value").forEach(function (el) {
+            el.textContent = copy;
+          });
+          document.querySelector(".answer-logo").classList.add("hedo");
+          document
+            .querySelector(".answer-logo")
+            .classList.remove("techno", "nomads", "barba");
           break;
         case 2:
-          copyElement.textContent = "Barbarians";
-          logoElement.className = "answer-logo barba";
+          copy = "Barbarians";
+          document.querySelectorAll(".answer-value").forEach(function (el) {
+            el.textContent = copy;
+          });
+          document.querySelector(".answer-logo").classList.add("barba");
+          document
+            .querySelector(".answer-logo")
+            .classList.remove("techno", "nomads", "hedo");
           break;
         case 3:
-          copyElement.textContent = "Nomads";
-          logoElement.className = "answer-logo nomads";
+          copy = "Nomads";
+          document.querySelectorAll(".answer-value").forEach(function (el) {
+            el.textContent = copy;
+          });
+          document.querySelector(".answer-logo").classList.add("nomads");
+          document
+            .querySelector(".answer-logo")
+            .classList.remove("techno", "barba", "hedo");
           break;
         case 4:
-          copyElement.textContent = "Technognostics";
-          logoElement.className = "answer-logo techno";
+          copy = "Technognostics";
+          document.querySelectorAll(".answer-value").forEach(function (el) {
+            el.textContent = copy;
+          });
+          document.querySelector(".answer-logo").classList.add("techno");
+          document
+            .querySelector(".answer-logo")
+            .classList.remove("nomads", "barba", "hedo");
           break;
         default:
-          copyElement.textContent = "Average is out of range.";
-      }
-    });
-
-  // Uncomment the following code if you need to handle reset functionality
-  // document.querySelector(".reset-quiz").addEventListener("click", function () {
-  //   document.querySelectorAll(".quiz-a").forEach(function (answer) {
-  //     answer.classList.remove("selected");
-  //   });
-  //   document.querySelector("div.surv_slider_nav.w-slider-nav div:nth-child(1)").dispatchEvent(new Event("tap"));
-  //   localStorage.clear();
-  // });
-
-  // Code for Webflow Slider
-  const l = document.querySelector("#quiz-slider .w-slider-arrow-left");
-  const r = document.querySelector("#quiz-slider .quiz-right-arrow");
-  document
-    .getElementById("quiz-slider")
-    .addEventListener("click", function (event) {
-      if (event.target.classList.contains("back-button")) {
-        l.dispatchEvent(new Event("tap"));
-      } else if (event.target.classList.contains("quiz-a")) {
-        r.dispatchEvent(new Event("tap"));
+          copy = "Average is out of range.";
       }
     });
 });
